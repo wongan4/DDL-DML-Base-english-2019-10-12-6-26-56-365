6aa54d03c2881af5176869acdf1e2c5c73eb7ec5
Andy Wong <andy.wong@oocl.com>
2020-01-09 17:24:31 +0800

    ans

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,46 @@
 -- **Database level:**
 -- Display all the database
+show databases;
 
 -- Enter a certain database
+use database_name;
 
 -- Create a database
+create database database_name;
 
 -- Create the database of the designated character set
+create database database_name character set charset_name collate collation_name;
 
 -- Display the creation information fo the database
+show create database database_name;
 
 -- Revise the codes of the database
+alter database database_name;
 
 -- Delete a database
+drop database database_name;
 
 -- **Table level**
 -- Revise table name
+alter table old_table_name rename to new_table_name;
 
 -- Revise the field's data type
+alter table table_name modify column column_name datatype;
 
 -- Revise field name
+alter table table_name rename column old_column_name to new_column_name;
 
 -- Add field
+alter table table_name add column_name datatype;
 
 -- Delete field
+alter table table_name drop column column_name;
 
 -- Revise the table's storage engine
+alter table table_name engine = engine_name;
 
 -- Delete the table's foreign key restriant
+alter table table_name drop foreign key key_name;
 
 -- Delete a table
+drop table table_name;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,11 @@
 -- Insert record
+insert into table_name (col1, col2, col3, ...) values (val1, val2, val3, ...);
 
 -- Revise record
+update table set col1=val1, col2=val2, ... where condition;
 
 --  Delete record
+delete from table_name where condition;
 
 -- Search record
+select * from table_name where condition;
